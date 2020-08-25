9e8fd9a1d744cb74454104b45652f304a2dc1e34
René Juan Rico Mendoza <rejurime@gmail.com>
2020-08-25 18:31:35 -0300

    Added more documentation in tgcgame

--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -5,6 +5,10 @@ using Microsoft.Xna.Framework.Input;
 
 namespace TGC.MonoGame.TP
 {
+    /// <summary>
+    /// Esta es la clase principal  del juego.
+    /// Inicialmente puede ser renombrado o copiado para hacer más ejemplos chicos, en el caso de copiar para que se ejecute el nuevo ejemplo deben cambiar la clase que ejecuta Program <see cref="Program.Main()" /> linea 10.
+    /// </summary>
     public class TGCGame : Game
     {
         public const string ContentFolder3D = "Models/";
@@ -14,10 +18,18 @@ namespace TGC.MonoGame.TP
         public const string ContentFolderSpriteFonts = "SpriteFonts/";
         public const string ContentFolderTextures = "Textures/";
 
+        /// <summary>
+        ///     Constructor del juego.
+        /// </summary>
         public TGCGame()
         {
+            // Maneja la configuracion y la administracion del dispositivo grafico.
             Graphics = new GraphicsDeviceManager(this);
+            // Descomentar para que el juego sea pantalla completa.
+            // Graphics.IsFullScreen = true;
+            // Carpeta raiz donde va a estar toda la Media.
             Content.RootDirectory = "Content";
+            // Hace que el mouse sea visible.
             IsMouseVisible = true;
         }
 
@@ -29,18 +41,23 @@ namespace TGC.MonoGame.TP
         private Matrix View { get; set; }
         private Matrix Projection { get; set; }
 
+        /// <summary>
+        /// Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
+        /// Escribir aquí todo el código de inicialización: todo procesamiento que podemos pre calcular para nuestro juego.
+        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            // La logica de inicializacion que no depende del contenido se recomienda poner en este metodo.
 
-            // Turn off the backface culling.
-            // This is done because off a problem in how the logo model is design.
-            // Once you start your game, it is not necessary to have it off.
+            // Apago el backface culling.
+            // Esto se hace por un problema en el diseno del modelo del logo de la materia.
+            // Una vez que empiecen su juego, esto no es mas necesario y lo pueden sacar.
             var rasterizerState = new RasterizerState();
             rasterizerState.CullMode = CullMode.None;
             GraphicsDevice.RasterizerState = rasterizerState;
+            // Seria hasta aca.
 
-            // Setup our graphics scene matrices
+            // Configuramos nuestras matrices de la escena.
             World = Matrix.CreateRotationY(MathHelper.Pi);
             View = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
             Projection =
@@ -49,37 +66,68 @@ namespace TGC.MonoGame.TP
             base.Initialize();
         }
 
+        /// <summary>
+        /// Se llama una sola vez, al principio cuando se ejecuta el ejemplo, despues de Initialize.
+        /// Escribir aquí todo el código de inicialización: cargar modelos, texturas, estructuras de optimización, todo procesamiento que podemos pre calcular para nuestro juego.
+        /// </summary>
         protected override void LoadContent()
         {
-            // TODO: use this.Content to load your game content here
+            // Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
             SpriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Load mesh.
+            // Cargo el modelo del logo.
             Model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
-            var modelEffect = (BasicEffect) Model.Meshes[0].Effects[0];
+            // Obtengo su efecto para cambiarle el color y activar la luz predeterminada que tiene MonoGame.
+            var modelEffect = (BasicEffect)Model.Meshes[0].Effects[0];
             modelEffect.DiffuseColor = Color.DarkBlue.ToVector3();
             modelEffect.EnableDefaultLighting();
+
+            base.LoadContent();
         }
 
+        /// <summary>
+        /// Se llama en cada frame.
+        /// Se debe escribir toda la lógica de computo del modelo, así como también verificar entradas del usuario y reacciones ante ellas.
+        /// </summary>
         protected override void Update(GameTime gameTime)
         {
-            // TODO: Add your update logic here
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
-                Keyboard.GetState().IsKeyDown(Keys.Escape))
+            // Aca deberiamos poner toda la logica de actualizacion del juego.
+
+            // Capturar Input teclado
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                //Salgo del juego.
                 Exit();
 
+            // Basado en el tiempo que paso se va generando una rotacion.
             Rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
 
             base.Update(gameTime);
         }
 
+        /// <summary>
+        /// Se llama cada vez que hay que refrescar la pantalla.
+        /// Escribir aquí todo el código referido al renderizado.
+        /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            // TODO: Add your drawing code here
+            // Aca deberiamos poner toda la logia de renderizado del juego.
             GraphicsDevice.Clear(Color.Black);
+
+            //Finalmente invocamos al draw del modelo.
             Model.Draw(World * Matrix.CreateRotationY(Rotation), View, Projection);
 
             base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Libero los recursos que se cargaron en el juego.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // Libero los recursos.
+            Content.Unload();
+
+            base.UnloadContent();
         }
     }
 }
