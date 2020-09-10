655033b78051621dee76b96d18f34d1f8f281bcc
Matias Szlajen <mszlajen@dblandit.com>
2020-09-10 20:36:53 -0300

    Add basic logic

--- TGC.MonoGame.InsaneGames/Entities/TGCito.cs
+++ TGC.MonoGame.InsaneGames/Entities/TGCito.cs
@@ -1,24 +1,34 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TGC.MonoGame.InsaneGames.Entities
 {
-    public class TGCito : IEnemy
+    public class TGCito : DrawableGameComponent, IEnemy
     {
+        private const string ModelName = "tgcito/tgcito-classic";
+        private Model Model;
+        private Matrix SpawnPoint;
+        private new TGCGame Game;
+
+        public TGCito(TGCGame game) : base(game)
+        {
+            Game = game;
+        }
+        public void Initialize(Matrix spawnPoint)
+        {
+            SpawnPoint = spawnPoint;
+        }
         public void Load()
         {
-
+            Model = Game.Content.Load<Model>(TGCGame.ContentFolder3D + ModelName);
         }
-        public void Update(GameTime gameTime)
+        public override void Update(GameTime gameTime)
         {
 
         }
-        public void Draw(GameTime gameTime)
+        public override void Draw(GameTime gameTime)
         {
-
-        }
-        public void Unload()
-        {
-
+            Model.Draw(SpawnPoint, Game.Camera.View, Game.Camera.Projection);
         }
     }
 }
