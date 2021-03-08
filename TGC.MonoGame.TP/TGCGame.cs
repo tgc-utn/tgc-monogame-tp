a5da0d3d7571c51e2774b49129f5f25e1e4a723d
René Juan Rico Mendoza <rejurime@gmail.com>
2021-03-08 01:31:43 -0300

    Remove basic effect (#10)
    * Changed BasicEffect to use a Custom Effect
    * Added Rider to gitignore
    * Added Bepu to nuget
    * Minor changes in the README

--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -13,7 +13,7 @@ namespace TGC.MonoGame.TP
     public class TGCGame : Game
     {
         public const string ContentFolder3D = "Models/";
-        public const string ContentFolderEffect = "Effects/";
+        public const string ContentFolderEffects = "Effects/";
         public const string ContentFolderMusic = "Music/";
         public const string ContentFolderSounds = "Sounds/";
         public const string ContentFolderSpriteFonts = "SpriteFonts/";
@@ -37,6 +37,7 @@ namespace TGC.MonoGame.TP
         private GraphicsDeviceManager Graphics { get; }
         private SpriteBatch SpriteBatch { get; set; }
         private Model Model { get; set; }
+        private Effect Effect { get; set; }
         private float Rotation { get; set; }
         private Matrix World { get; set; }
         private Matrix View { get; set; }
@@ -44,7 +45,7 @@ namespace TGC.MonoGame.TP
 
         /// <summary>
         ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
-        ///     Escribir aquí todo el código de inicialización: todo procesamiento que podemos pre calcular para nuestro juego.
+        ///     Escribir aqui el codigo de inicializacion: el procesamiento que podemos pre calcular para nuestro juego.
         /// </summary>
         protected override void Initialize()
         {
@@ -59,7 +60,7 @@ namespace TGC.MonoGame.TP
             // Seria hasta aca.
 
             // Configuramos nuestras matrices de la escena.
-            World = Matrix.CreateRotationY(MathHelper.Pi);
+            World = Matrix.Identity;
             View = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
             Projection =
                 Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
@@ -69,8 +70,8 @@ namespace TGC.MonoGame.TP
 
         /// <summary>
         ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo, despues de Initialize.
-        ///     Escribir aqui el codigo de inicializacion: cargar modelos, texturas, estructuras de optimizacion, el
-        ///     procesamiento que podemos pre calcular para nuestro juego.
+        ///     Escribir aqui el codigo de inicializacion: cargar modelos, texturas, estructuras de optimizacion, el procesamiento
+        ///     que podemos pre calcular para nuestro juego.
         /// </summary>
         protected override void LoadContent()
         {
@@ -79,17 +80,24 @@ namespace TGC.MonoGame.TP
 
             // Cargo el modelo del logo.
             Model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
-            // Obtengo su efecto para cambiarle el color y activar la luz predeterminada que tiene MonoGame.
-            var modelEffect = (BasicEffect) Model.Meshes[0].Effects[0];
-            modelEffect.DiffuseColor = Color.DarkBlue.ToVector3();
-            modelEffect.EnableDefaultLighting();
+
+            // Cargo un efecto basico propio declarado en el Content pipeline.
+            // En el juego no pueden usar BasicEffect de MG, deben usar siempre efectos propios.
+            Effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
+
+            // Asigno el efecto que cargue a cada parte del mesh.
+            // Un modelo puede tener mas de 1 mesh internamente.
+            foreach (var mesh in Model.Meshes)
+                // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
+            foreach (var meshPart in mesh.MeshParts)
+                meshPart.Effect = Effect;
 
             base.LoadContent();
         }
 
         /// <summary>
         ///     Se llama en cada frame.
-        ///     Se debe escribir toda la lógica de computo del modelo, así como también verificar entradas del usuario y reacciones
+        ///     Se debe escribir toda la logica de computo del modelo, asi como tambien verificar entradas del usuario y reacciones
         ///     ante ellas.
         /// </summary>
         protected override void Update(GameTime gameTime)
@@ -109,17 +117,25 @@ namespace TGC.MonoGame.TP
 
         /// <summary>
         ///     Se llama cada vez que hay que refrescar la pantalla.
-        ///     Escribir aquí todo el código referido al renderizado.
+        ///     Escribir aqui el codigo referido al renderizado.
         /// </summary>
         protected override void Draw(GameTime gameTime)
         {
             // Aca deberiamos poner toda la logia de renderizado del juego.
             GraphicsDevice.Clear(Color.Black);
 
-            //Finalmente invocamos al draw del modelo.
-            Model.Draw(World * Matrix.CreateRotationY(Rotation), View, Projection);
+            // Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
+            Effect.Parameters["View"].SetValue(View);
+            Effect.Parameters["Projection"].SetValue(Projection);
+            Effect.Parameters["DiffuseColor"].SetValue(Color.DarkBlue.ToVector3());
+            var rotationMatrix = Matrix.CreateRotationY(Rotation);
 
-            base.Draw(gameTime);
+            foreach (var mesh in Model.Meshes)
+            {
+                World = mesh.ParentBone.Transform * rotationMatrix;
+                Effect.Parameters["World"].SetValue(World);
+                mesh.Draw();
+            }
         }
 
         /// <summary>
