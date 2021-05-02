0406d2d2e9d0f5a30aa277d8c34a6cead2d80692
MaxBurnit <37699106+MaxBurnit@users.noreply.github.com>
2021-05-01 22:10:53 -0300

    puse una bolillita y un arito en el espacio

---
+++ TGC.MonoGame.TP/Cameras/SimpleCamera.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace TGC.MonoGame.Samples.Cameras
+{
+    /// <summary>
+    ///     Camera with simple movement.
+    /// </summary>
+    public class SimpleCamera : Camera
+    {
+        /// <summary>
+        ///     Forward direction of the camera.
+        /// </summary>
+        public readonly Vector3 DefaultWorldFrontVector = Vector3.Forward;
+
+        /// <summary>
+        ///     The direction that is "up" from the camera's point of view.
+        /// </summary>
+        public readonly Vector3 DefaultWorldUpVector = Vector3.Up;
+
+        /// <summary>
+        ///     Camera with simple movement to be able to move in the 3D world, which has the up vector in (0,1,0) and the forward
+        ///     vector in (0,0,-1).
+        /// </summary>
+        /// <param name="aspectRatio">Aspect ratio, defined as view space width divided by height.</param>
+        /// <param name="position">The position of the camera.</param>
+        /// <param name="speed">The speed of movement.</param>
+        /// <param name="angle">The angle of movement.</param>
+        public SimpleCamera(float aspectRatio, Vector3 position, float speed, float angle) : base(aspectRatio)
+        {
+            BuildView(position, speed, angle);
+        }
+
+        /// <summary>
+        ///     Camera with simple movement to be able to move in the 3D world, which has the up vector in (0,1,0) and the forward
+        ///     vector in (0,0,-1).
+        /// </summary>
+        /// <param name="aspectRatio">Aspect ratio, defined as view space width divided by height.</param>
+        /// <param name="position">The position of the camera.</param>
+        /// <param name="speed">The speed of movement.</param>
+        /// <param name="angle">The angle of movement.</param>
+        /// <param name="nearPlaneDistance">Distance to the near view plane.</param>
+        /// <param name="farPlaneDistance">Distance to the far view plane.</param>
+        public SimpleCamera(float aspectRatio, Vector3 position, float speed, float angle, float nearPlaneDistance,
+            float farPlaneDistance) : base(aspectRatio, nearPlaneDistance, farPlaneDistance)
+        {
+            BuildView(position, speed, angle);
+        }
+
+        /// <summary>
+        ///     Value with which the camera is going to move.
+        /// </summary>
+        public float Speed { get; set; }
+
+        /// <summary>
+        ///     Value with which the camera is going to move the angle.
+        /// </summary>
+        public float Angle { get; set; }
+
+        /// <summary>
+        ///     Build view matrix and update the internal directions.
+        /// </summary>
+        /// <param name="position">The position of the camera.</param>
+        /// <param name="speed">The speed of movement.</param>
+        /// <param name="angle">The angle of movement.</param>
+        private void BuildView(Vector3 position, float speed, float angle)
+        {
+            Position = position;
+            FrontDirection = DefaultWorldFrontVector;
+            Speed = speed;
+            Angle = angle;
+            View = Matrix.CreateLookAt(Position, Position + FrontDirection, DefaultWorldUpVector);
+        }
+
+        /// <inheritdoc />
+        public override void Update(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+            var time = Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
+
+            // Check for input to rotate the camera.
+            var pitch = 0f;
+            var turn = 0f;
+
+            if (keyboardState.IsKeyDown(Keys.Up))
+                pitch += time * Angle;
+
+            if (keyboardState.IsKeyDown(Keys.Down))
+                pitch -= time * Angle;
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+                turn += time * Angle;
+
+            if (keyboardState.IsKeyDown(Keys.Right))
+                turn -= time * Angle;
+
+            RightDirection = Vector3.Cross(DefaultWorldUpVector, FrontDirection);
+            var flatFront = Vector3.Cross(RightDirection, DefaultWorldUpVector);
+
+            var pitchMatrix = Matrix.CreateFromAxisAngle(RightDirection, pitch);
+            var turnMatrix = Matrix.CreateFromAxisAngle(DefaultWorldUpVector, turn);
+
+            var tiltedFront = Vector3.TransformNormal(FrontDirection, pitchMatrix * turnMatrix);
+
+            // Check angle so we can't flip over.
+            if (Vector3.Dot(tiltedFront, flatFront) > 0.001f) FrontDirection = Vector3.Normalize(tiltedFront);
+
+            // Check for input to move the camera around.
+            if (keyboardState.IsKeyDown(Keys.W))
+                Position += FrontDirection * time * Speed;
+
+            if (keyboardState.IsKeyDown(Keys.S))
+                Position -= FrontDirection * time * Speed;
+
+            if (keyboardState.IsKeyDown(Keys.A))
+                Position += RightDirection * time * Speed;
+
+            if (keyboardState.IsKeyDown(Keys.D))
+                Position -= RightDirection * time * Speed;
+
+            View = Matrix.CreateLookAt(Position, Position + FrontDirection, DefaultWorldUpVector);
+        }
+    }
+}
--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using TGC.MonoGame.Samples.Cameras;
+using TGC.MonoGame.Samples.Geometries;
 
 namespace TGC.MonoGame.TP
 {
@@ -33,7 +35,7 @@ namespace TGC.MonoGame.TP
             // Hace que el mouse sea visible.
             IsMouseVisible = true;
         }
-
+        private Camera Camera { get; set; }
         private GraphicsDeviceManager Graphics { get; }
         private SpriteBatch SpriteBatch { get; set; }
         private Model Model { get; set; }
@@ -42,6 +44,10 @@ namespace TGC.MonoGame.TP
         private Matrix World { get; set; }
         private Matrix View { get; set; }
         private Matrix Projection { get; set; }
+        private SpherePrimitive Sphere { get; set; }
+        private Vector3 SpherePosition { get; set; }
+        private TorusPrimitive Torus { get; set; }
+        private Vector3 TorusPosition { get; set; }
 
         /// <summary>
         ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
@@ -58,12 +64,18 @@ namespace TGC.MonoGame.TP
             rasterizerState.CullMode = CullMode.None;
             GraphicsDevice.RasterizerState = rasterizerState;
             // Seria hasta aca.
+            Camera = new TargetCamera(GraphicsDevice.Viewport.AspectRatio, new Vector3(0, 20, 60), Vector3.Zero);
 
-            // Configuramos nuestras matrices de la escena.
+            Sphere = new SpherePrimitive(GraphicsDevice, 10);
+            SpherePosition = new Vector3(0, 0, 0);
+
+            Torus = new TorusPrimitive(GraphicsDevice, 20,1);
+            TorusPosition = new Vector3(0, -2, -10);
+            /*// Configuramos nuestras matrices de la escena.
             World = Matrix.Identity;
             View = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
             Projection =
-                Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
+                Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);*/
 
             base.Initialize();
         }
@@ -75,7 +87,7 @@ namespace TGC.MonoGame.TP
         /// </summary>
         protected override void LoadContent()
         {
-            // Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
+            /*// Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
             SpriteBatch = new SpriteBatch(GraphicsDevice);
 
             // Cargo el modelo del logo.
@@ -92,7 +104,7 @@ namespace TGC.MonoGame.TP
             foreach (var meshPart in mesh.MeshParts)
                 meshPart.Effect = Effect;
 
-            base.LoadContent();
+            base.LoadContent();*/
         }
 
         /// <summary>
@@ -109,10 +121,10 @@ namespace TGC.MonoGame.TP
                 //Salgo del juego.
                 Exit();
 
-            // Basado en el tiempo que paso se va generando una rotacion.
+            /*// Basado en el tiempo que paso se va generando una rotacion.
             Rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
 
-            base.Update(gameTime);
+            base.Update(gameTime);*/
         }
 
         /// <summary>
@@ -122,9 +134,9 @@ namespace TGC.MonoGame.TP
         protected override void Draw(GameTime gameTime)
         {
             // Aca deberiamos poner toda la logia de renderizado del juego.
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.BlueViolet);
 
-            // Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
+            /*// Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
             Effect.Parameters["View"].SetValue(View);
             Effect.Parameters["Projection"].SetValue(Projection);
             Effect.Parameters["DiffuseColor"].SetValue(Color.DarkBlue.ToVector3());
@@ -135,12 +147,26 @@ namespace TGC.MonoGame.TP
                 World = mesh.ParentBone.Transform * rotationMatrix;
                 Effect.Parameters["World"].SetValue(World);
                 mesh.Draw();
-            }
+            }*/
+            DrawGeometry(Sphere, SpherePosition, 0, 0, 0);
+            DrawGeometry(Torus, TorusPosition, 0, MathHelper.Pi/2, 0);
+        }
+
+        private void DrawGeometry(GeometricPrimitive geometry, Vector3 position, float yaw, float pitch, float roll)
+        {
+            var effect = geometry.Effect;
+
+            effect.World = Matrix.CreateFromYawPitchRoll(yaw, pitch, roll) * Matrix.CreateTranslation(position);
+            effect.View = Camera.View;
+            effect.Projection = Camera.Projection;
+
+            geometry.Draw(effect);
         }
 
         /// <summary>
         ///     Libero los recursos que se cargaron en el juego.
         /// </summary>
+
         protected override void UnloadContent()
         {
             // Libero los recursos.
