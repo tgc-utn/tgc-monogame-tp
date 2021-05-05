6803a0aec2b66b63a11cbc355daa492b1aa93727
Sebas <cabral.k00@gmail.com>
2021-05-05 17:46:33 -0300

    Camera Ajustments

--- TGC.MonoGame.TP/Sources/Camera.cs
+++ TGC.MonoGame.TP/Sources/Camera.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace TGC.MonoGame.TP
 {
-    class Camera
+    internal class Camera
     {
         private Vector3 position = new Vector3(0f, 0f, 150f);
         public Matrix View { get; private set; }
@@ -13,93 +13,74 @@ namespace TGC.MonoGame.TP
 
         private const float walkSpeed = 100f;
         private const float runSpeed = walkSpeed * 5;
-        private const float mouseSensitivity = 5f;
+        private const float mouseSensitivity = 2f;
 
         private const float fieldOfView = MathHelper.PiOver4;
         private const float nearPlaneDistance = 0.1f;
         private const float farPlaneDistance = 2000f;
 
-        private float pitch;
-        private float yaw = -90f;
+        private float pitch, yaw = -90f;
         private Vector3 frontDirection = -Vector3.UnitZ;
         private Vector3 rightDirection = Vector3.Right;
         private Vector3 upDirection = Vector3.Up;
 
-        private bool changed;
         private Vector2 pastMousePosition;
         private Point screenCenter;
 
-        public void Initialize(GraphicsDevice graphicsDevice)
+        internal void Initialize(GraphicsDevice graphicsDevice)
         {
             screenCenter = new Point(graphicsDevice.Viewport.Width / 2, graphicsDevice.Viewport.Height / 2);
-            Projection = Matrix.CreatePerspectiveFieldOfView(fieldOfView, graphicsDevice.Viewport.AspectRatio, nearPlaneDistance, farPlaneDistance);
-            UpdateViewMatrix();
+            Projection = CreateProjectionMatrix(graphicsDevice);
+            View = CreateViewMatrix();
         }
 
-        public void Update(GameTime gameTime)
+        internal void Update(GameTime gameTime)
         {
-            var elapsedTime = (float) gameTime.ElapsedGameTime.TotalSeconds;
+            float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             ProcessKeyboard(elapsedTime);
             ProcessMouseMovement(elapsedTime);
-            UpdateViewMatrix();
+            View = CreateViewMatrix();
         }
 
-        private void UpdateViewMatrix()
-        {
-            if (changed)
-            {
-                View = Matrix.CreateLookAt(position, position + frontDirection, upDirection);
-                changed = false;
-            }
-        }
+        // Matrix
+
+        private Matrix CreateProjectionMatrix(GraphicsDevice graphicsDevice) => Matrix.CreatePerspectiveFieldOfView(fieldOfView, graphicsDevice.Viewport.AspectRatio, nearPlaneDistance, farPlaneDistance);
+        private Matrix CreateViewMatrix() => Matrix.CreateLookAt(position, position + frontDirection, upDirection);
+
+        // Keyboard
 
         private float MovementSpeed() => Input.Turbo() ? runSpeed : walkSpeed;
 
         private void ProcessKeyboard(float elapsedTime)
         {
             Vector3 inputDirection = Input.HorizontalAxis() * rightDirection + Input.ForwardAxis() * frontDirection + Input.VerticalAxis() * upDirection;
-            changed = !Equals(inputDirection, Vector3.Zero);
-
-            inputDirection.Normalize();
-            position += changed ? inputDirection * MovementSpeed() * elapsedTime : Vector3.Zero;
+            bool changed = !Equals(inputDirection, Vector3.Zero);
+            position += changed ? Vector3.Normalize(inputDirection) * MovementSpeed() * elapsedTime : Vector3.Zero;
         }
+
+        // Mouse
+
+        private Vector2 CurrentMousePosition() => Mouse.GetState().Position.ToVector2();
 
         private void ProcessMouseMovement(float elapsedTime)
         {
-            var mouseState = Mouse.GetState();
+            Vector2 mouseDelta = (CurrentMousePosition() - pastMousePosition) * mouseSensitivity * elapsedTime;
+            yaw += mouseDelta.X;
+            pitch -= mouseDelta.Y;
+            pitch = Math.Clamp(pitch, -89.9f, 89.9f);
 
-            Mouse.SetCursor(MouseCursor.No);
-                var mouseDelta = mouseState.Position.ToVector2() - pastMousePosition;
-                mouseDelta *= mouseSensitivity * elapsedTime;
-
-                yaw += mouseDelta.X;
-                pitch -= mouseDelta.Y;
-
-                if (pitch > 89.0f)
-                    pitch = 89.0f;
-                if (pitch < -89.0f)
-                    pitch = -89.0f;
-
-                changed = true;
-                UpdateCameraVectors();
-
+            UpdateCameraVectors();
             Mouse.SetPosition(screenCenter.X, screenCenter.Y);
-
-            pastMousePosition = Mouse.GetState().Position.ToVector2();
+            pastMousePosition = CurrentMousePosition();
         }
 
         private void UpdateCameraVectors()
         {
-            // Calculate the new Front vector
-            Vector3 tempFront;
-            tempFront.X = MathF.Cos(MathHelper.ToRadians(yaw)) * MathF.Cos(MathHelper.ToRadians(pitch));
-            tempFront.Y = MathF.Sin(MathHelper.ToRadians(pitch));
-            tempFront.Z = MathF.Sin(MathHelper.ToRadians(yaw)) * MathF.Cos(MathHelper.ToRadians(pitch));
-
-            frontDirection = Vector3.Normalize(tempFront);
-
-            // Also re-calculate the Right and Up vector
-            // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+            frontDirection = Vector3.Normalize(new Vector3(
+                MathF.Cos(MathHelper.ToRadians(yaw)) * MathF.Cos(MathHelper.ToRadians(pitch)),
+                MathF.Sin(MathHelper.ToRadians(pitch)),
+                MathF.Sin(MathHelper.ToRadians(yaw)) * MathF.Cos(MathHelper.ToRadians(pitch))
+            ));
             rightDirection = Vector3.Normalize(Vector3.Cross(frontDirection, Vector3.Up));
             upDirection = Vector3.Normalize(Vector3.Cross(rightDirection, frontDirection));
         }
