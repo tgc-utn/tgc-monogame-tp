986e5b5dbbec6952e054ce1d5a311c42d81ac49f
Sebas <cabral.k00@gmail.com>
2023-04-12 23:00:02 -0300

    Carga de Auto y Quads

---
+++ TGC.MonoGame.TP/FollowCamera.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TGC.MonoGame.TP
+{
+    class FollowCamera
+    {
+        private const float AxisDistanceToTarget = 1000f;
+
+        private const float AngleFollowSpeed = 0.015f;
+
+        private const float AngleThreshold = 0.85f;
+
+        public Matrix Projection { get; private set; }
+
+        public Matrix View { get; private set; }
+
+        private Vector3 CurrentRightVector { get; set; } = Vector3.Right;
+
+        private float RightVectorInterpolator { get; set; } = 0f;
+
+        private Vector3 PastRightVector { get; set; } = Vector3.Right;
+
+        public FollowCamera(float aspectRatio)
+        {
+            Projection = Matrix.CreatePerspectiveFieldOfView(MathF.PI / 3f, aspectRatio, 0.1f, 100000f);
+        }
+
+        public void Update(GameTime gameTime, Matrix followedWorld)
+        {
+            var elapsedTime = Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
+            
+            var followedPosition = followedWorld.Translation;
+
+            var followedRight = followedWorld.Right;
+
+            if (Vector3.Dot(followedRight, PastRightVector) > AngleThreshold)
+            {
+                RightVectorInterpolator += elapsedTime * AngleFollowSpeed;
+
+                RightVectorInterpolator = MathF.Min(RightVectorInterpolator, 1f);
+
+                CurrentRightVector = Vector3.Lerp(CurrentRightVector, followedRight, RightVectorInterpolator * RightVectorInterpolator);
+            }
+            else
+                RightVectorInterpolator = 0f;
+
+            PastRightVector = followedRight;
+            
+            var offsetedPosition = followedPosition 
+                + CurrentRightVector * AxisDistanceToTarget
+                + Vector3.Up * AxisDistanceToTarget;
+
+            var forward = (followedPosition - offsetedPosition);
+            forward.Normalize();
+
+            var right = Vector3.Cross(forward, Vector3.Up);
+            var cameraCorrectUp = Vector3.Cross(right, forward);
+
+            View = Matrix.CreateLookAt(offsetedPosition, followedPosition, cameraCorrectUp);
+        }
+    }
+}
--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -36,30 +36,24 @@ namespace TGC.MonoGame.TP
         private GraphicsDeviceManager Graphics { get; }
         private SpriteBatch SpriteBatch { get; set; }
         private Model Model { get; set; }
+        private Model CarModel { get; set; }
         private Effect Effect { get; set; }
         private float Rotation { get; set; }
         private Matrix World { get; set; }
         private Matrix View { get; set; }
         private Matrix Projection { get; set; }
+        private QuadPrimitive Floor { get; set; }
+        private QuadPrimitive Floor2 { get; set; }
+        private FollowCamera Camera { get; set; }
 
-        /// <summary>
-        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
-        ///     Escribir aqui el codigo de inicializacion: el procesamiento que podemos pre calcular para nuestro juego.
-        /// </summary>
         protected override void Initialize()
         {
-            // La logica de inicializacion que no depende del contenido se recomienda poner en este metodo.
-
-            // Apago el backface culling.
-            // Esto se hace por un problema en el diseno del modelo del logo de la materia.
-            // Una vez que empiecen su juego, esto no es mas necesario y lo pueden sacar.
             var rasterizerState = new RasterizerState();
             rasterizerState.CullMode = CullMode.None;
             GraphicsDevice.RasterizerState = rasterizerState;
-            // Seria hasta aca.
 
-            // Configuramos nuestras matrices de la escena.
             World = Matrix.Identity;
+            Camera = new FollowCamera(GraphicsDevice.Viewport.AspectRatio);
             View = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
             Projection =
                 Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
@@ -67,88 +61,52 @@ namespace TGC.MonoGame.TP
             base.Initialize();
         }
 
-        /// <summary>
-        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo, despues de Initialize.
-        ///     Escribir aqui el codigo de inicializacion: cargar modelos, texturas, estructuras de optimizacion, el procesamiento
-        ///     que podemos pre calcular para nuestro juego.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
             SpriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // Cargo el modelo del logo.
             Model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
+            CarModel = Content.Load<Model>("Models/RacingCar");
+            Floor = new QuadPrimitive(GraphicsDevice);
+            Floor2 = new QuadPrimitive(GraphicsDevice);
 
-            // Cargo un efecto basico propio declarado en el Content pipeline.
-            // En el juego no pueden usar BasicEffect de MG, deben usar siempre efectos propios.
             Effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
-
-            // Asigno el efecto que cargue a cada parte del mesh.
-            // Un modelo puede tener mas de 1 mesh internamente.
-            foreach (var mesh in Model.Meshes)
-            {
-                // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
-                foreach (var meshPart in mesh.MeshParts)
-                {
-                    meshPart.Effect = Effect;
-                }
-            }
-
             base.LoadContent();
         }
 
-        /// <summary>
-        ///     Se llama en cada frame.
-        ///     Se debe escribir toda la logica de computo del modelo, asi como tambien verificar entradas del usuario y reacciones
-        ///     ante ellas.
-        /// </summary>
         protected override void Update(GameTime gameTime)
         {
-            // Aca deberiamos poner toda la logica de actualizacion del juego.
-
-            // Capturar Input teclado
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-            {
-                //Salgo del juego.
                 Exit();
-            }
-
-            // Basado en el tiempo que paso se va generando una rotacion.
+            
             Rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
-
+            World = Matrix.CreateScale(0.5f) * Matrix.CreateRotationY(Rotation) * Matrix.CreateTranslation(0f, 50f, 0f);
+            Camera.Update(gameTime, World);
             base.Update(gameTime);
         }
 
-        /// <summary>
-        ///     Se llama cada vez que hay que refrescar la pantalla.
-        ///     Escribir aqui el codigo referido al renderizado.
-        /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            // Aca deberiamos poner toda la logia de renderizado del juego.
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.White);
 
-            // Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
-            Effect.Parameters["View"].SetValue(View);
-            Effect.Parameters["Projection"].SetValue(Projection);
+            Effect.Parameters["View"].SetValue(Camera.View);
+            Effect.Parameters["Projection"].SetValue(Camera.Projection);
             Effect.Parameters["DiffuseColor"].SetValue(Color.DarkBlue.ToVector3());
-            var rotationMatrix = Matrix.CreateRotationY(Rotation);
+            Effect.Parameters["World"].SetValue(World*Matrix.CreateScale(1000f, 0f, 1000f));
+            Floor.Draw(Effect);
+            CarModel.Draw(World, Camera.View, Camera.Projection);
 
-            foreach (var mesh in Model.Meshes)
-            {
-                World = mesh.ParentBone.Transform * rotationMatrix;
-                Effect.Parameters["World"].SetValue(World);
-                mesh.Draw();
-            }
+            Effect.Parameters["View"].SetValue(Camera.View);
+            Effect.Parameters["Projection"].SetValue(Camera.Projection);
+            Effect.Parameters["DiffuseColor"].SetValue(Color.DarkGreen.ToVector3());
+            Effect.Parameters["World"].SetValue(World*Matrix.CreateScale(1000f, 0f, 1000f)*Matrix.CreateRotationY(MathHelper.PiOver4)*Matrix.CreateTranslation(10f, 50f, 10f) );
+            Floor2.Draw(Effect);
+
+            var rotationMatrix = Matrix.CreateRotationY(Rotation);
         }
 
-        /// <summary>
-        ///     Libero los recursos que se cargaron en el juego.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // Libero los recursos.
             Content.Unload();
 
             base.UnloadContent();
