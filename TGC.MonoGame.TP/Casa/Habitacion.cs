eb4b05075fe513e2efd94fd97ac305ceabba3b36
manurocck <manuelnielsen.5g@gmail.com>
2023-04-18 14:22:36 -0300

    Retoques en diseño

--- TGC.MonoGame.TP/Casa/Habitacion.cs
+++ TGC.MonoGame.TP/Casa/Habitacion.cs
@@ -13,10 +13,8 @@ namespace TGC.MonoGame.TP
         private Vector3 PosicionInicial;
         private Piso Piso;
         private List<Pared> Paredes;
-        private List<IElementoDinamico> ElementosDinamicos;
-        private List<IElemento> Elementos;
-
-        private Pared ParedQueAnda;
+        private List<IElementoDinamico> MueblesDinamicos;
+        private List<IElemento> Muebles;
 
         //Ancho y Alto en Cantidad de Baldosas
         private Habitacion(int ancho, int alto, Vector3 posicionInicial)
@@ -25,36 +23,27 @@ namespace TGC.MonoGame.TP
             Alto = alto;
             PosicionInicial = posicionInicial;
 
-            ElementosDinamicos = new List<IElementoDinamico>();
-            Elementos = new List<IElemento>();
+            MueblesDinamicos = new List<IElementoDinamico>();
+            Muebles = new List<IElemento>();
             Piso = new Piso(ancho, alto, posicionInicial); // Se carga el default
 
             Paredes = new List<Pared>();
             Paredes.Add(Pared.Arriba (10, 10, Vector3.Zero));
-           /*  Paredes.Add(Pared.Derecha   (ancho, alto, posicionInicial + new Vector3(0, 0, ancho * 5000f)));          
-            Paredes.Add(Pared.Izquierda    (ancho, alto, posicionInicial + new Vector3(ancho * 5000f, 0, ancho * 5000f)));
-            Paredes.Add(Pared.Abajo     (ancho, alto, posicionInicial + new Vector3(0, 0, ancho * 5000f))); */          
-
-            ParedQueAnda = Pared.Arriba(10, 10, Vector3.Zero);
+            Paredes.Add(Pared.Abajo (10, 10, Vector3.Zero));
+            Paredes.Add(Pared.Izquierda (10, 10, Vector3.Zero));
+            Paredes.Add(Pared.Derecha (10, 10, Vector3.Zero));
         }
-        public void AddDinamico( IElementoDinamico elem ){
-            elem.newPosicionInicial(PosicionInicial);
-            ElementosDinamicos.Add(elem);
-        }
-        private void AddElemento( IElemento elem ){
-            elem.newPosicionInicial(PosicionInicial);
-            Elementos.Add(elem);
-        }
-
         public static Habitacion Oficina(int ancho, int alto, Vector3 posicionInicial){
             var oficina = new Habitacion(alto, ancho, posicionInicial);
 
             oficina.Piso.Oficina();
+            oficina.AddElemento(new Mueble("Misil", 5f,new Vector3(0f,0f,0f), new Vector3(0f,0f,0f)));
             oficina.AddElemento(new Mueble("Silla-Oficina", 10f,new Vector3(100f,0f,100f), new Vector3(-MathHelper.PiOver2,MathHelper.PiOver4,0f)));
 //            oficina.AddElemento(new Mueble("Gabinete", 10f,Vector3.Zero, new Vector3(0f,0f,MathHelper.Pi)));
             oficina.AddElemento(new Mueble("Cafe-Rojo", 10f,new Vector3(100f,50f,100f), new Vector3(-MathHelper.PiOver2,0f,0f)));
 
-            
+
+
             return oficina;
         }
         public static Habitacion Cocina(int ancho, int alto, Vector3 posicionInicial){
@@ -68,7 +57,7 @@ namespace TGC.MonoGame.TP
         }
         public static Habitacion Principal(int ancho, int alto, Vector3 posicionInicial){
             Habitacion principal = new Habitacion(ancho,alto,posicionInicial);
-            #region CargaElementosDinámicos
+            #region CargaMueblesDinámicos
             var posicionesAutosIA = new Vector3(0f,0f,300f);           
             for(int i=0; i<20; i++){
                 var escala = 0.04f * Random.Shared.NextSingle() + 0.04f;
@@ -77,7 +66,7 @@ namespace TGC.MonoGame.TP
             }
             #endregion
             
-            #region CargaElementosEstáticos
+            #region CargaMueblesEstáticos
             for(int i = 0 ; i<100*20 ; i+=100){
                 principal.AddElemento(new Mueble("Chair", 10f, new Vector3(   i , 40f , -30f ), Vector3.Zero));
             }
@@ -90,21 +79,29 @@ namespace TGC.MonoGame.TP
             return principal;
         }
 
+        public void AddDinamico( IElementoDinamico elem ){
+            elem.newPosicionInicial(PosicionInicial);
+            MueblesDinamicos.Add(elem);
+        }
+        private void AddElemento( IElemento elem ){
+            elem.newPosicionInicial(PosicionInicial);
+            Muebles.Add(elem);
+        }
+
         public void Load(ContentManager content)
         {
-            ParedQueAnda.Load(content);
             Piso.Load(content);
 
             foreach(var pared in Paredes)
                 pared.Load(content);
-            foreach(var elemento in ElementosDinamicos)
+            foreach(var elemento in MueblesDinamicos)
                 elemento.Load(content);
-            foreach(var elemento in Elementos)
+            foreach(var elemento in Muebles)
                 elemento.Load(content);  
         }
 
         public void Update(GameTime gameTime, KeyboardState keyboardState){
-            foreach(var e in ElementosDinamicos){
+            foreach(var e in MueblesDinamicos){
                 e.Update(gameTime, keyboardState);
             }
             return;
@@ -114,12 +111,13 @@ namespace TGC.MonoGame.TP
         {
             Piso.Draw(view, projection);
 
+
             foreach(var pared in Paredes)
                 pared.Draw(view, projection);
 
-            foreach(var elemento in ElementosDinamicos)
+            foreach(var elemento in MueblesDinamicos)
                 elemento.Draw(view, projection);
-            foreach(var elemento in Elementos)
+            foreach(var elemento in Muebles)
                 elemento.Draw(view, projection);
         }
     }
--- TGC.MonoGame.TP/Diseño/IElemento.cs
+++ TGC.MonoGame.TP/Diseño/IElemento.cs
@@ -7,28 +7,62 @@ using Microsoft.Xna.Framework.Input;
 namespace TGC.MonoGame.TP{
     /// Crea un elemento estático en la posición del vector PosicionInicial
     public abstract class IElemento  {
-        public Matrix World {get; set;}
-        private Model Model;
+        public Matrix World = Matrix.Identity;
+        public Model Model;
         public Vector3 PosicionInicial {get; set;}
         public Vector3 rotacion;
+        //AGREGAR ESCALA
         //AGREGAR EFFECT
-        public string Origen {get; set;} // '/Models/Carpeta/Modelo.fbx'
+        public string Origen = null; // '/Models/Carpeta/Modelo.fbx'
         
         public IElemento(string path, Vector3 posicionInicial, Vector3 rotacion){
-            if(path!=null)
+            if(path!="")
                 Origen = path;
-            Matrix Rotation = Matrix.CreateRotationX(rotacion.X) * Matrix.CreateRotationY(rotacion.Y) * Matrix.CreateRotationZ(rotacion.Z);
-            World = Rotation * Matrix.CreateTranslation(posicionInicial);
             PosicionInicial = posicionInicial;
+
+            Rotar(rotacion);
+            Trasladar(posicionInicial);
+        }
+        public IElemento(string path, Vector3 posicionInicial, Vector3 rotacion, float escala){
+            if(path!="")
+                Origen = path;
+            PosicionInicial = posicionInicial;
+            
+            Escalar(escala);
+            Rotar(rotacion);
+            Trasladar(posicionInicial);
+        }
+        public IElemento(Vector3 posicionInicial, Vector3 rotacion){
+            PosicionInicial = posicionInicial;
+            Rotar(rotacion);
+            Trasladar(posicionInicial);
+        }
+        public IElemento(Vector3 posicionInicial, Vector3 rotacion, float escala){
+            Escalar(escala);
+
+            PosicionInicial = posicionInicial;
+            Rotar(rotacion);
+            Trasladar(posicionInicial);
         }
 
+    
+        private void Escalar(float escala){
+            World *= Matrix.CreateScale(escala);
+        }
+        private void Rotar(Vector3 rotacion){
+            World *= Matrix.CreateRotationX(rotacion.X) * Matrix.CreateRotationY(rotacion.Y) * Matrix.CreateRotationZ(rotacion.Z);
+        }
+        private void Trasladar(Vector3 posicionInicial){
+            World = World * Matrix.CreateTranslation(posicionInicial);
+        }
+    
         public void newPosicionInicial(Vector3 posicionInicial){
             PosicionInicial = posicionInicial;
             World *= Matrix.CreateTranslation(posicionInicial);
         }
         public void Load(ContentManager contentManager){
-            if(Origen!=null)
-            Model = contentManager.Load<Model>(Origen);
+            if(Origen!=null && Origen!="")
+                Model = contentManager.Load<Model>(Origen);
             return; 
         }
         public void Draw (Matrix view, Matrix projection){
