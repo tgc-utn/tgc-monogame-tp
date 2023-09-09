b06ae009350cb61731306e9b77b16c5a1a1c4e00
absku <marcospedaci@gmail.com>
2023-09-09 19:13:03 -0300

    FIRST COMMIT

---
+++ TGC.MonoGame.TP/Builder/Builder.cs
@@ -0,0 +1,10 @@
+﻿namespace TGC.MonoGame.TP.Builder;
+
+public class Builder
+{
+    internal static void Draw()
+    {
+        /*var builder = new Builder();
+        builder.BuildContent();*/
+    }
+}
