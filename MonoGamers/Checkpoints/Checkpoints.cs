a09c378d4b7a7c98d83b6381558275036572ef47
TobiasDucrot <tducrot@frba.utn.edu.ar>
2023-09-18 15:00:00 -0300

    Avance checkpoints

---
+++ MonoGamers/Checkpoints/Checkpoints.cs
@@ -0,0 +1,22 @@
+using Microsoft.Xna.Framework;
+using MonoGamers.Collisions;
+
+namespace MonoGamers.Checkpoints
+{
+    public class Checkpoint 
+    {
+        public Vector3 Position {get;set;}
+        public BoundingBox BoundingBox {get;set;}
+        public Checkpoint (Vector3 position)
+        {
+            this.Position = position;
+            var world = Matrix.CreateScale(6,6,6) * Matrix.CreateTranslation(position);
+            this.BoundingBox = BoundingVolumesExtensions.FromMatrix(world);
+        }
+        public bool IsWithinBounds(Vector3 position)
+        {
+            var BoundingSphere = new BoundingSphere(position, 5f);
+            return BoundingBox.Intersects(BoundingSphere);
+        }
+    }
+}
