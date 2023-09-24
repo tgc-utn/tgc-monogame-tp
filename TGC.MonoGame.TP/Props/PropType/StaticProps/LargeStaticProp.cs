d3adac9b630e741196e6b890ae64591bdf83a2d7
Juan Martin Vazquez <juavazquez@frba.utn.edu.ar>
2023-09-24 16:06:26 -0300

    Adding collisions format

---
+++ TGC.MonoGame.TP/Props/PropType/StaticProps/LargeStaticProp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using TGC.MonoGame.TP.Collisions;
+using TGC.MonoGame.TP.References;
+
+namespace TGC.MonoGame.TP.Props.PropType.StaticProps;
+
+public class LargeStaticProp : StaticProp
+{
+    public LargeStaticProp(PropReference modelReference) : base(modelReference) {}
+
+    public LargeStaticProp(PropReference modelReference, Vector3 position) : base(modelReference, position) {}
+
+
+    public override void CollidedWith(ICollidable other)
+    {
+        other.CollidedWithLargeProp();
+        Console.WriteLine("Me choco un: " + other.GetType().Name);
+    }
+}
