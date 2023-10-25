27a9e04ccf66603a818b161b2ccbffbdadd29c56
mpedaci <mpedaci@utn.frba.edu.ar>
2023-10-24 22:34:49 -0300

    Add menu

---
+++ TGC.MonoGame.TP/Menu/MainMenu.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Content;
+using TGC.MonoGame.TP.Cameras;
+using TGC.MonoGame.TP.Types;
+using TGC.MonoGame.TP.Types.References;
+using TGC.MonoGame.TP.Utils;
+
+namespace TGC.MonoGame.TP.Menu;
+
+public class MainMenu
+{
+    private ButtonsGrid Buttons;
+    private Texture2D Logo;
+    private int _screenWidth;
+    private int _screenHeight;
+
+    // Background
+    private Camera _camera;
+    private Camera _environmentCamera;
+    private SpriteFont Font;
+    public SpriteBatch SpriteBatch { get; set; }
+    public GraphicsDevice GraphicsDevice { get; set; }
+    
+    public bool loaded = false;
+
+    public MainMenu (GraphicsDevice graphicsDevice, GameState gameState)
+        {
+            GraphicsDevice = graphicsDevice;
+            SpriteBatch = new SpriteBatch(graphicsDevice);
+            loaded = true;
+            
+            _screenWidth = graphicsDevice.Viewport.Width;
+            _screenHeight = graphicsDevice.Viewport.Height;
+
+            var buttons = new List<Button>
+            {
+                new ("Comenzar", GameStatus.NormalGame),
+                new ("Modo Dios", GameStatus.GodModeGame),
+                new ("Salir", GameStatus.Exit),
+            };
+            Buttons = new ButtonsGrid(gameState, _screenWidth/2, _screenHeight/2, buttons);
+        }
+
+        /// <summary>
+        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo, despues de Initialize.
+        ///     Escribir aqui el codigo de inicializacion: cargar modelos, texturas, estructuras de optimizacion, el procesamiento
+        ///     que podemos pre calcular para nuestro juego.
+        /// </summary>
+        public void LoadContent(ContentManager content)
+        {
+            Logo = content.Load<Texture2D>(Utils.Textures.Menu.MenuImage.Path);
+            Font = content.Load<SpriteFont>(Utils.Fonts.Fonts.Arial.Path);
+            Buttons.LoadContent(content);
+        }
+
+        public void Update()
+        {
+            if (!loaded) 
+                SpriteBatch = new SpriteBatch(GraphicsDevice);
+            Buttons.Update(Mouse.GetState());
+        }
+
+        private void DrawEnvironment(Camera camera)
+        {
+            GraphicsDevice.Clear(Color.Black);
+            // GraphicsDevice.DepthStencilState = DepthStencilState.Default;
+            GraphicsDevice.SetRenderTarget(null);
+        }
+        
+        public void Draw(GameStatus gameStatus)
+        {
+            if (loaded)
+            {
+                DrawEnvironment(_environmentCamera);
+                var destRectangle = new Rectangle(0, 0, Logo.Width, Logo.Height);
+                SpriteBatch.Begin();
+                SpriteBatch.Draw(Logo, Vector2.Zero, Color.White);
+                if (gameStatus == GameStatus.DeathMenu)
+                {
+                    var text = "Perdiste!";
+                    var size = Font.MeasureString(text);
+                    SpriteBatch.DrawString(Font, text, new Vector2((_screenWidth - size.Y)/2, 20), Color.Red);
+                }
+                SpriteBatch.End();
+                Buttons.Draw(SpriteBatch);
+            }
+        }
+
+        public void Dispose()
+        {
+            loaded = false;
+            
+            // SpriteBatch.Dispose();
+        }
+}
---
+++ TGC.MonoGame.TP/Utils/Textures/Menu.cs
@@ -0,0 +1,13 @@
+﻿using TGC.MonoGame.TP.Types.References;
+
+namespace TGC.MonoGame.TP.Utils.Textures;
+
+public static class Menu
+{
+    public static readonly TextureReference ButtonNormal = new TextureReference(
+        $"{ContentFolder.Images}/button");
+    public static readonly TextureReference ButtonHover = new TextureReference(
+        $"{ContentFolder.Images}/hoverbutton");
+    public static readonly TextureReference MenuImage = new TextureReference(
+        $"{ContentFolder.Images}/main");
+}
