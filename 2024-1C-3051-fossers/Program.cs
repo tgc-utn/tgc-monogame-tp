bc959f1cadd9e5e880caf49b027da5011a3a433b
nicolau <marcosnicolau2003@gmail.com>
2024-04-23 21:57:21 -0300

    base config

---
+++ 2024-1C-3051-fossers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using WarSteel;
+
+public static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        using (var game = new Game())
+            game.Run();
+    }
+}
