4cb5853e6799b79c963d5b98641c07548a461128
Lautaro Moyano <36833412+nullspace1@users.noreply.github.com>
2024-05-13 00:23:58 -0300

    Refactor: Basic downward acceleration

--- 2024-1C-3051-fossers/src/Prefabs/Tank.cs
+++ 2024-1C-3051-fossers/src/Prefabs/Tank.cs
@@ -19,13 +19,12 @@ public class Tank : Entity
         Model model = ContentRepoManager.Instance().GetModel("Tanks/Panzer/Panzer");
         float width = ModelUtils.GetWidth(model);
         float height = ModelUtils.GetHeight(model);
-        AddComponent(new RigidBody(Transform,new BoxCollider(Transform,new Vector3(-width,-height,-height), new Vector3(width,height,height)),20,Matrix.Identity, false));
-        AddComponent(new LightComponent(Color.White,new Vector3(2000,0,0)));
+        AddComponent(new RigidBody(Transform, new Collider(), 20, Matrix.Identity));
+        AddComponent(new LightComponent(Color.White, new Vector3(2000, 0, 0)));
     }
 
     public override void Initialize(Scene scene)
     {
-        
         base.Initialize(scene);
     }
 
--- 2024-1C-3051-fossers/src/Scenes/SceneProcessors/PhysicsProcessor.cs
+++ 2024-1C-3051-fossers/src/Scenes/SceneProcessors/PhysicsProcessor.cs
@@ -38,167 +38,20 @@ public class PhysicsProcessor : ISceneProcessor
         foreach (var rigidBody in SimulatedBodies.Values)
         {
             IntegrateVelocities(rigidBody, dt);
-
-            DetectColissions(rigidBody);
-
-            ResolveColissions(rigidBody);
         }
+
     }
 
     public void IntegrateVelocities(RigidBody rigidBody, float dt)
     {
 
-        if (rigidBody.isFixed) return;
+        Vector3 forces = Gravity;
+        Vector3 torques = Vector3.Zero;
 
-        rigidBody.AddForce(Gravity);
-
-        Vector3 dv = (rigidBody.Forces - rigidBody.Velocity * Drag) / rigidBody.Mass * dt;
-        Vector3 dw = Vector3.Transform(rigidBody.Torques - rigidBody.AngularVelocity * AngularDrag, rigidBody.InvInertiaTensor) * dt;
-
-        rigidBody.Velocity += dv;
-        rigidBody.AngularVelocity += dw;
-
-        rigidBody.Transform.Pos += rigidBody.Velocity * dt;
-
-        Quaternion dq = Quaternion.Multiply(new Quaternion(dw.X, dw.Y, dw.Z, 0), dt / 2) * rigidBody.Transform.Orientation;
-        rigidBody.Transform.Orientation += dq;
-        rigidBody.Transform.Orientation = Quaternion.Multiply(rigidBody.Transform.Orientation, 1 / rigidBody.Transform.Orientation.Length());
-
-        rigidBody.Forces = Vector3.Zero;
-        rigidBody.Torques = Vector3.Zero;
+        rigidBody.IntegrateVelocities(forces,torques,dt);
 
     }
 
-    public void DetectColissions(RigidBody rigidBody)
-    {
-
-        foreach (var r in SimulatedBodies.Values)
-        {
-
-            if (ColliderBodies[r].Contains(rigidBody)) break;
-
-            if (rigidBody != r && Collides(rigidBody, r))
-            {
-                ColliderBodies[rigidBody].Add(r);
-            }
-
-        }
-    }
-
-    public void ResolveColissions(RigidBody A)
-    {
-
-        // TODO : Al chano lo balearon por menos. Ver si hay una solucion mejor.
-        foreach (RigidBody B in ColliderBodies[A])
-        {
-
-            switch (A.Collider.GetColliderType())
-            {
-
-                case ColliderType.BOX:
-                    switch (B.Collider.GetColliderType())
-                    {
-                        case ColliderType.BOX:
-                            BoxBoxCollision(A, B);
-                            break;
-                        case ColliderType.SPHERE:
-                            BoxSphereCollision(A, B);
-                            break;
-                    }
-                    break;
-
-                case ColliderType.SPHERE:
-                    switch (B.Collider.GetColliderType())
-                    {
-                        case ColliderType.BOX:
-                            BoxSphereCollision(B, A);
-                            break;
-                        case ColliderType.SPHERE:
-                            SphereSphereCollision(A, B);
-                            break;
-                    }
-                    break;
-
-            }
-
-        }
-    }
-
-    private void SphereSphereCollision(RigidBody a, RigidBody b)
-    {
-        SphereCollider aCollider = (SphereCollider)a.Collider;
-        SphereCollider bCollider = (SphereCollider)b.Collider;
-
-        if (aCollider.GetBoundingSphere().Intersects(bCollider.GetBoundingSphere()))
-        {
-            Vector3 normal = bCollider.GetCenter() - aCollider.GetCenter();
-            normal.Normalize();
-
-            b.Velocity = (2 * normal * Vector3.Dot(normal, b.Velocity) - b.Velocity) *  (1 - a.Mass / (b.Mass + a.Mass)) ;
-            a.Velocity = (2 * -normal * Vector3.Dot(-normal, a.Velocity) - a.Velocity) *  (1 - b.Mass / (a.Mass + b.Mass)) ;
-
-        }
-
-
-    }
-
-    private void BoxSphereCollision(RigidBody a, RigidBody b)
-    {
-        throw new NotImplementedException();
-    }
-
-    private void BoxBoxCollision(RigidBody a, RigidBody b)
-    {
-
-        BoxCollider colliderA = (BoxCollider)a.Collider;
-        BoxCollider colliderB = (BoxCollider)b.Collider;
-
-        // Check for AABB overlap
-        if (colliderA.GetBoundingBox().Intersects(colliderB.GetBoundingBox()))
-        {
-            // Calculate collision normal
-            Vector3 collisionNormal = CalculateCollisionNormal(a, b);
-            a.Velocity = (2 * collisionNormal * Vector3.Dot(collisionNormal,a.Velocity) - a.Velocity) * (1 - a.Mass / (b.Mass + a.Mass)) ;
-            b.Velocity = (- 2 * collisionNormal * Vector3.Dot(collisionNormal,b.Velocity) - b.Velocity) *  (1 - b.Mass / (a.Mass + b.Mass)) ;
-        }
-
-    }
-
-
-
-    private Vector3 CalculateCollisionNormal(RigidBody a, RigidBody b)
-    {
-
-        BoxCollider aC = (BoxCollider)a.Collider;
-        BoxCollider bC = (BoxCollider)b.Collider;
-
-        BoundingBox bbA = aC.GetBoundingBox();
-        BoundingBox bbB = bC.GetBoundingBox();
-
-        float xOverlap = Math.Min(bbA.Max.X, bbB.Max.X) - Math.Max(bbA.Min.X, bbB.Min.X);
-        float yOverlap = Math.Min(bbA.Max.Y, bbB.Max.Y) - Math.Max(bbA.Min.Y, bbB.Min.Y);
-        float zOverlap = Math.Min(bbA.Max.Z, bbB.Max.Z) - Math.Max(bbA.Min.Z, bbB.Min.Z);
-
-        // Determine axis of least overlap
-        Vector3 collisionNormal = Vector3.Zero;
-        float minOverlap = float.MaxValue;
-        if (xOverlap < minOverlap)
-        {
-            minOverlap = xOverlap;
-            collisionNormal = Vector3.UnitX;
-        }
-        if (yOverlap < minOverlap)
-        {
-            minOverlap = yOverlap;
-            collisionNormal = Vector3.UnitY;
-        }
-        if (zOverlap < minOverlap)
-        {
-            collisionNormal = Vector3.UnitZ;
-        }
-
-        return collisionNormal;
-    }
 
     public void RemoveRigidBody(RigidBody body)
     {
@@ -211,9 +64,6 @@ public class PhysicsProcessor : ISceneProcessor
         ColliderBodies.Add(body, new List<RigidBody>());
     }
 
-    public static bool Collides(RigidBody r1, RigidBody r2)
-    {
-        return r1.Collider.GetBoundingBox().Intersects(r2.Collider.GetBoundingBox());
-    }
+
 
 }
