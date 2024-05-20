f113332b3358282cca595276abe4b2c76cae0ad6
nicolau <marcosnicolau2003@gmail.com>
2024-05-20 19:41:33 -0300

    player class

---
+++ 2024-1C-3051-fossers/src/Scenes/Main/Player/Controls.cs
@@ -0,0 +1,63 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using WarSteel.Entities;
+using WarSteel.Utils;
+
+namespace WarSteel.Scenes.Main;
+
+public class PlayerControls : IComponent
+{
+    DynamicBody rb;
+    float Damage = 100;
+    float BulletForce = 100;
+    bool IsReloading = false;
+    int ReloadingTimeInMs = 500;
+
+
+    public void UpdateEntity(Entity self, GameTime gameTime, Scene scene)
+    {
+        if (Keyboard.GetState().IsKeyDown(Keys.W))
+        {
+            // model is reversed
+            rb.ApplyForce(self.Transform.GetWorld().Backward * 100);
+        }
+        if (Keyboard.GetState().IsKeyDown(Keys.S))
+        {
+            rb.ApplyForce(self.Transform.GetWorld().Forward * 100);
+        }
+        if (Keyboard.GetState().IsKeyDown(Keys.A))
+        {
+            rb.ApplyTorque(self.Transform.GetWorld().Up * 350f);
+        }
+        if (Keyboard.GetState().IsKeyDown(Keys.D))
+        {
+            rb.ApplyTorque(self.Transform.GetWorld().Up * -350f);
+        }
+        if (isClickingLMB())
+        {
+            if (IsReloading) return;
+            Vector3 CameraPos = scene.GetCamera().Transform.Pos;
+            Vector3 Dir = new(-CameraPos.X, CameraPos.Y, -CameraPos.Z);
+            Bullet bullet = new Bullet("player-bullet", Damage, self.Transform.Pos + new Vector3(Dir.X, 200, Dir.Z), Dir, BulletForce);
+            bullet.Initialize(scene);
+            scene.AddEntity(bullet);
+            IsReloading = true;
+            Timer.Timeout(ReloadingTimeInMs, () => IsReloading = false);
+        }
+    }
+
+    private bool isClickingLMB()
+    {
+        if (Mouse.GetState().LeftButton == ButtonState.Pressed)
+            return true;
+
+        return false;
+    }
+
+    public void Initialize(Entity self, Scene scene)
+    {
+        rb = self.GetComponent<DynamicBody>();
+    }
+
+    public void Destroy(Entity self, Scene scene) { }
+}
