b2bf91e0dc28b42b2f4aacf8b5498b92ef76f7c4
nicolau <marcosnicolau2003@gmail.com>
2024-05-20 19:42:13 -0300

    refactor main scene and adding map components again

--- 2024-1C-3051-fossers/src/Scenes/Main/Main.cs
+++ 2024-1C-3051-fossers/src/Scenes/Main/Main.cs
@@ -2,10 +2,8 @@
 using Microsoft.Xna.Framework;
 using WarSteel.Entities;
 using WarSteel.Common;
-using WarSteel.Entities.Map;
 using WarSteel.Scenes.SceneProcessors;
-using Microsoft.Xna.Framework.Graphics;
-using WarSteel.Entities.Components;
+
 
 namespace WarSteel.Scenes.Main;
 
@@ -13,11 +11,11 @@ public class MainScene : Scene
 {
     public MainScene(GraphicsDeviceManager Graphics) : base(Graphics)
     {
+
     }
 
     public override void Initialize()
     {
-
         LightProcessor light = new LightProcessor(Color.AliceBlue);
 
         light.AddLightSource(new LightSource(Color.Red, new Vector3(0, 500, 0)));
@@ -28,19 +26,15 @@ public class MainScene : Scene
         Camera camera = new(new Vector3(0, 800, -500), Graphics.GraphicsDevice.Viewport.AspectRatio, MathHelper.PiOver2, 0.1f, 300000f);
         camera.AddComponent(new MouseController(0.01f));
 
-        Entity tank = new Tank("player");
-        tank.AddComponent(new PlayerControls());
-        Entity ground = new Ground();
-        tank.Transform.Rotate(new Vector3(0, 180, 0));
+        Player player = new Player();
+        player.Initialize(this);
 
-        AddEntity(tank);
-        AddEntity(ground);
+        Map map = new Map();
+        map.Initialize(this);
 
         camera.Follow(GetEntityByName("player"));
-
         SetCamera(camera);
+
         base.Initialize();
     }
-
-
 }
