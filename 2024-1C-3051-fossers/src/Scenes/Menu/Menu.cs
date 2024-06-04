72aed6ffa73040537eec3b810102579dc41a1763
nicolau <marcosnicolau2003@gmail.com>
2024-06-04 15:26:07 -0300

    fix some stuff about namespaces

--- 2024-1C-3051-fossers/src/Scenes/Menu/Menu.cs
+++ 2024-1C-3051-fossers/src/Scenes/Menu/Menu.cs
@@ -1,6 +1,8 @@
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using WarSteel.Screens;
+using WarSteel.Screens.MainMenu;
 
 namespace WarSteel.Scenes.Main;
 
--- 2024-1C-3051-fossers/src/Scenes/SceneProcessors/UIProcessor.cs
+++ 2024-1C-3051-fossers/src/Scenes/SceneProcessors/UIProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using WarSteel.Scenes;
+using WarSteel.Screens;
 using WarSteel.UIKit;
 
 public class UIProcessor : ISceneProcessor
