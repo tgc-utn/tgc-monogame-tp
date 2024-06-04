caa6e483e2b98292229ac0b32e282ec37a42dcc9
nicolau <marcosnicolau2003@gmail.com>
2024-06-04 16:00:01 -0300

    added debug logs to event manager

--- 2024-1C-3051-fossers/src/Utils/EventManager.cs
+++ 2024-1C-3051-fossers/src/Utils/EventManager.cs
@@ -6,41 +6,48 @@ public class EventManager
     private static EventManager _instance;
     public static EventManager Instance => _instance ?? (_instance = new EventManager());
 
-    private Dictionary<string, Action<object>> _eventDictionary;
+    private Dictionary<string, List<Delegate>> _eventDictionary;
 
     private EventManager()
     {
-        _eventDictionary = new Dictionary<string, Action<object>>();
+        _eventDictionary = new Dictionary<string, List<Delegate>>();
     }
 
-    public void StartListening(string eventName, Action<object> listener)
+    public void StartListening<T>(string eventName, Action<T> listener)
     {
-        if (_eventDictionary.TryGetValue(eventName, out var thisEvent))
+        if (!_eventDictionary.ContainsKey(eventName))
         {
-            thisEvent += listener;
-            _eventDictionary[eventName] = thisEvent;
+            _eventDictionary[eventName] = new List<Delegate>();
         }
-        else
-        {
-            thisEvent = new Action<object>(listener);
-            _eventDictionary.Add(eventName, thisEvent);
-        }
-    }
 
-    public void StopListening(string eventName, Action<object> listener)
-    {
-        if (_eventDictionary.TryGetValue(eventName, out var thisEvent))
+        _eventDictionary[eventName].Add(listener);
+
+        if (Constants.DEBUG_MODE)
         {
-            thisEvent -= listener;
-            _eventDictionary[eventName] = thisEvent;
+            Console.WriteLine($"Listener added for event: {eventName}");
         }
     }
 
-    public void TriggerEvent(string eventName, object parameter = null)
+    public void TriggerEvent<T>(string eventName, T eventArgs)
     {
-        if (_eventDictionary.TryGetValue(eventName, out var thisEvent))
+        if (_eventDictionary.TryGetValue(eventName, out var listeners))
         {
-            thisEvent.Invoke(parameter);
+            foreach (Delegate listener in listeners)
+            {
+                var action = listener as Action<T>;
+                action?.Invoke(eventArgs);
+            }
+
+            if (Constants.DEBUG_MODE)
+            {
+                Console.WriteLine($"Listener removed for event: {eventName}");
+            }
+        }
+
+        // Check for debug mode and log the event if true
+        if (Constants.DEBUG_MODE)
+        {
+            Console.WriteLine($"Event triggered: {eventName}");
         }
     }
 }
