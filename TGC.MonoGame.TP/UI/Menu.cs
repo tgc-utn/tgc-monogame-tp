6c176987d90a1dc91456ec2cc7028afd072de93e
Federico Bertani <lbertani@dguiaf-gcba.gov.ar>
2024-06-30 07:32:37 -0300

    Agrego pantalla de título y menú

---
+++ TGC.MonoGame.TP/UI/Menu.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGC.MonoGame.TP.UI
+{
+    internal class Menu : Text
+    {
+        Option selected;
+
+        public enum Option
+        {
+            Resume,
+            Restart,
+            GodMode,
+            SelectStage,
+            Exit
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public override void Draw(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, SpriteFont font)
+        {
+
+            spriteBatch.Begin();
+
+            var position = CenterText(graphicsDevice, font, "Resume", 1f);
+            spriteBatch.DrawString(font, "Resume", position, selected == Option.Resume ? Color.White : Color.Gray);
+            position.Y += 25;
+
+            spriteBatch.DrawString(font, "Restart", position, selected == Option.Restart ? Color.White : Color.Gray);
+            position.Y += 25;
+
+            spriteBatch.DrawString(font, "God Mode", position, selected == Option.GodMode ? Color.White : Color.Gray);
+            position.Y += 30;
+
+            spriteBatch.DrawString(font, "Select stage", position, selected == Option.SelectStage ? Color.White : Color.Gray);
+            position.Y += 30;
+
+            spriteBatch.DrawString(font, "Exit", position, selected == Option.Exit ? Color.White : Color.Gray);
+
+            spriteBatch.End();
+
+        }
+    }
+}
---
+++ TGC.MonoGame.TP/UI/TitleScreen.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TGC.MonoGame.TP.UI
+{
+    internal class TitleScreen : Text
+    {
+
+        const string titleText = "Marble It Down!";
+        const float titleScale = 5f;
+
+        const string pressStartText = "Press any key";
+        const float pressStartScale = 1f;
+
+        public override void Draw(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, SpriteFont font)
+        {
+            spriteBatch.Begin();
+
+            var titlePosition = CenterText(graphicsDevice, font, titleText, titleScale);
+            DrawStringWithShadow(spriteBatch, font, pressStartText, titlePosition, Color.Yellow, titleScale);
+
+            var pressStartPosition = CenterText(graphicsDevice, font, titleText, titleScale) + new Vector2(0,200);
+            DrawStringWithShadow(spriteBatch, font, pressStartText, pressStartPosition, Color.White, pressStartScale); 
+
+            spriteBatch.End();
+        }
+
+    }
+}
