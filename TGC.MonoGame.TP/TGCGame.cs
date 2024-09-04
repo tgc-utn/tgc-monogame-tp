efda2a49d146d659f5b4e97b8c1eebb73ede5920
tomi2108 <tominegrette@hotmail.com>
2024-09-04 19:00:09 -0300

    add GeometricPrimitives and SpherePrimitive

--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -3,159 +3,98 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace TGC.MonoGame.TP
-{
-    /// <summary>
-    ///     Esta es la clase principal del juego.
-    ///     Inicialmente puede ser renombrado o copiado para hacer mas ejemplos chicos, en el caso de copiar para que se
-    ///     ejecute el nuevo ejemplo deben cambiar la clase que ejecuta Program <see cref="Program.Main()" /> linea 10.
-    /// </summary>
-    public class TGCGame : Game
-    {
-        public const string ContentFolder3D = "Models/";
-        public const string ContentFolderEffects = "Effects/";
-        public const string ContentFolderMusic = "Music/";
-        public const string ContentFolderSounds = "Sounds/";
-        public const string ContentFolderSpriteFonts = "SpriteFonts/";
-        public const string ContentFolderTextures = "Textures/";
+namespace TGC.MonoGame.TP {
+public class TGCGame : Game {
+  public const string ContentFolder3D = "Models/";
+  public const string ContentFolderEffects = "Effects/";
+  public const string ContentFolderMusic = "Music/";
+  public const string ContentFolderSounds = "Sounds/";
+  public const string ContentFolderSpriteFonts = "SpriteFonts/";
+  public const string ContentFolderTextures = "Textures/";
 
-        /// <summary>
-        ///     Constructor del juego.
-        /// </summary>
-        public TGCGame()
-        {
-            // Maneja la configuracion y la administracion del dispositivo grafico.
-            Graphics = new GraphicsDeviceManager(this);
-            
-            Graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
-            Graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 100;
-            
-            // Para que el juego sea pantalla completa se puede usar Graphics IsFullScreen.
-            // Carpeta raiz donde va a estar toda la Media.
-            Content.RootDirectory = "Content";
-            // Hace que el mouse sea visible.
-            IsMouseVisible = true;
-        }
+  public TGCGame() {
+    Graphics = new GraphicsDeviceManager(this);
 
-        private GraphicsDeviceManager Graphics { get; }
-        private SpriteBatch SpriteBatch { get; set; }
-        private Model Model { get; set; }
-        private Effect Effect { get; set; }
-        private float Rotation { get; set; }
-        private Matrix World { get; set; }
-        private Matrix View { get; set; }
-        private Matrix Projection { get; set; }
+    Graphics.PreferredBackBufferWidth =
+        GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
+    Graphics.PreferredBackBufferHeight =
+        GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 100;
 
-        /// <summary>
-        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
-        ///     Escribir aqui el codigo de inicializacion: el procesamiento que podemos pre calcular para nuestro juego.
-        /// </summary>
-        protected override void Initialize()
-        {
-            // La logica de inicializacion que no depende del contenido se recomienda poner en este metodo.
+    Content.RootDirectory = "Content";
+    IsMouseVisible = true;
+  }
 
-            // Apago el backface culling.
-            // Esto se hace por un problema en el diseno del modelo del logo de la materia.
-            // Una vez que empiecen su juego, esto no es mas necesario y lo pueden sacar.
-            var rasterizerState = new RasterizerState();
-            rasterizerState.CullMode = CullMode.None;
-            GraphicsDevice.RasterizerState = rasterizerState;
-            // Seria hasta aca.
+  private GraphicsDeviceManager Graphics { get; }
+  private SpriteBatch SpriteBatch { get; set; }
+  private Effect Effect { get; set; }
+  private Matrix World { get; set; }
+  private Matrix View { get; set; }
+  private Matrix Projection { get; set; }
 
-            // Configuramos nuestras matrices de la escena.
-            World = Matrix.Identity;
-            View = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
-            Projection =
-                Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
+  private Tp.SpherePrimitive PlayerSphere { get; set; }
+  private Vector3 PlayerPosition = Vector3.Zero;
+  private float PlayerSpeed = 2f;
 
-            base.Initialize();
-        }
+  protected override void Initialize() {
+    var rasterizerState = new RasterizerState();
+    rasterizerState.CullMode = CullMode.None;
+    GraphicsDevice.RasterizerState = rasterizerState;
+    World = Matrix.Identity;
+    View = Matrix.CreateLookAt(Vector3.UnitZ * 3, Vector3.Zero, Vector3.Up);
+    Projection = Matrix.CreatePerspectiveFieldOfView(
+        MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
 
-        /// <summary>
-        ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo, despues de Initialize.
-        ///     Escribir aqui el codigo de inicializacion: cargar modelos, texturas, estructuras de optimizacion, el procesamiento
-        ///     que podemos pre calcular para nuestro juego.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            // Aca es donde deberiamos cargar todos los contenido necesarios antes de iniciar el juego.
-            SpriteBatch = new SpriteBatch(GraphicsDevice);
+    base.Initialize();
+  }
 
-            // Cargo el modelo del logo.
-            Model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
+  protected override void LoadContent() {
+    SpriteBatch = new SpriteBatch(GraphicsDevice);
+    PlayerSphere = new Tp.SpherePrimitive(GraphicsDevice, 1, 16);
 
-            // Cargo un efecto basico propio declarado en el Content pipeline.
-            // En el juego no pueden usar BasicEffect de MG, deben usar siempre efectos propios.
-            Effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
+    Effect = Content.Load<Effect>(ContentFolderEffects + "BasicShader");
 
-            // Asigno el efecto que cargue a cada parte del mesh.
-            // Un modelo puede tener mas de 1 mesh internamente.
-            foreach (var mesh in Model.Meshes)
-            {
-                // Un mesh puede tener mas de 1 mesh part (cada 1 puede tener su propio efecto).
-                foreach (var meshPart in mesh.MeshParts)
-                {
-                    meshPart.Effect = Effect;
-                }
-            }
+    base.LoadContent();
+  }
 
-            base.LoadContent();
-        }
+  protected override void Update(GameTime gameTime) {
 
-        /// <summary>
-        ///     Se llama en cada frame.
-        ///     Se debe escribir toda la logica de computo del modelo, asi como tambien verificar entradas del usuario y reacciones
-        ///     ante ellas.
-        /// </summary>
-        protected override void Update(GameTime gameTime)
-        {
-            // Aca deberiamos poner toda la logica de actualizacion del juego.
+    float dt = Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
+    var keyboardState = Keyboard.GetState();
 
-            // Capturar Input teclado
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-            {
-                //Salgo del juego.
-                Exit();
-            }
-            
-            // Basado en el tiempo que paso se va generando una rotacion.
-            Rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
+    if (keyboardState.IsKeyDown(Keys.Escape))
+      Exit();
 
-            World = Matrix.CreateRotationY(Rotation);
+    if (keyboardState.IsKeyDown(Keys.W))
+      PlayerPosition.Z -= PlayerSpeed * dt;
 
-            base.Update(gameTime);
-        }
+    if (keyboardState.IsKeyDown(Keys.S))
+      PlayerPosition.Z += PlayerSpeed * dt;
 
-        /// <summary>
-        ///     Se llama cada vez que hay que refrescar la pantalla.
-        ///     Escribir aqui el codigo referido al renderizado.
-        /// </summary>
-        protected override void Draw(GameTime gameTime)
-        {
-            // Aca deberiamos poner toda la logia de renderizado del juego.
-            GraphicsDevice.Clear(Color.Black);
+    if (keyboardState.IsKeyDown(Keys.D))
+      PlayerPosition.X += PlayerSpeed * dt;
 
-            // Para dibujar le modelo necesitamos pasarle informacion que el efecto esta esperando.
-            Effect.Parameters["View"].SetValue(View);
-            Effect.Parameters["Projection"].SetValue(Projection);
-            Effect.Parameters["DiffuseColor"].SetValue(Color.DarkBlue.ToVector3());
+    if (keyboardState.IsKeyDown(Keys.A))
+      PlayerPosition.X -= PlayerSpeed * dt;
 
-            foreach (var mesh in Model.Meshes)
-            {
-                Effect.Parameters["World"].SetValue(mesh.ParentBone.Transform * World);
-                mesh.Draw();
-            }
-        }
+    World = Matrix.CreateTranslation(PlayerPosition);
 
-        /// <summary>
-        ///     Libero los recursos que se cargaron en el juego.
-        /// </summary>
-        protected override void UnloadContent()
-        {
-            // Libero los recursos.
-            Content.Unload();
+    base.Update(gameTime);
+  }
 
-            base.UnloadContent();
-        }
-    }
+  protected override void Draw(GameTime gameTime) {
+    GraphicsDevice.Clear(Color.Black);
+
+    Effect.Parameters["World"].SetValue(World);
+    Effect.Parameters["View"].SetValue(View);
+    Effect.Parameters["Projection"].SetValue(Projection);
+    Effect.Parameters["DiffuseColor"].SetValue(Color.DarkBlue.ToVector3());
+
+    PlayerSphere.Draw(Effect);
+  }
+
+  protected override void UnloadContent() {
+    Content.Unload();
+    base.UnloadContent();
+  }
+}
 }
